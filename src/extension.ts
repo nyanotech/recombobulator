a6157d0a169d6502d1581503692b82478ca8ab35
nyanotech <nyanotechnology@gmail.com>
2022-03-23 10:49:10 +0000

    Break out editor interaction into generic function

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,30 @@
 import * as vscode from 'vscode';
 
+function base64(a: string) {
+	return Buffer.from(a).toString('base64'); // TODO - handle different character encodings
+}
+
+function applyEdit(transform: Function) {
+	const editor = vscode.window.activeTextEditor;
+
+	if (editor) {
+		const document = editor.document;
+
+		editor.edit(editBuilder => {
+			editor.selections.forEach(selection => {
+				const text = document.getText(selection);
+				const processed = transform(text);
+				editBuilder.replace(selection, processed);
+			});
+		});
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Recombobulator is activated!');
 
 	context.subscriptions.push(vscode.commands.registerCommand('recombobulator.base64', () => {
-		const editor = vscode.window.activeTextEditor;
-
-		if (editor) {
-			const document = editor.document;
-
-			editor.edit(editBuilder => {
-				editor.selections.forEach(selection => {
-					const text = document.getText(selection);
-					const processed = Buffer.from(text).toString('base64'); // TODO - handle different character encodings
-					editBuilder.replace(selection, processed);
-				});
-			});
-		}
-
+		applyEdit(base64);
 		vscode.window.showInformationMessage('base64-encoded the selection!');
 	}));
 }
